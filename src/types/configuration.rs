c54690fc5da797ebff96cf9ee27d87edb8d3a268
Gui Gessele <gagessele@outlook.com.br>
2022-07-03 20:49:49 -0300

    Added KeyValue struct

--- src/types/configuration.rs
+++ src/types/configuration.rs
@@ -1,1 +1,12 @@
-pub struct KeyValue {}
+use super::utils::{CiString500Type, CiString50Type};
+
+/// Contains information about a specific configuration key.
+/// It is returned in GetConfiguration.conf.
+pub struct KeyValue {
+    key: CiString50Type,
+    /// False if the value can be set with the
+    /// ChangeConfiguration message
+    readonly: bool,
+    /// If key is known but not set, this field may be absent.
+    value: Option<CiString500Type>,
+}
