890bde1c18f9a372558176b0e46569c824e0473a
Gui Gessele <gagessele@outlook.com.br>
2022-07-03 14:38:57 -0300

    Removed id_tag_info moduled and updated dependencies

--- src/types/authorization_data.rs
+++ src/types/authorization_data.rs
@@ -1,5 +1,6 @@
-use super::id_tag_info::IdTagInfo;
-use super::id_token::IdToken;
+use chrono::{DateTime, Utc};
+
+use super::enumerations::AuthorizationStatus;
 
 pub struct AuthorizationData {
     /// The identifier to with this authorization applies
@@ -11,4 +12,52 @@ pub struct AuthorizationData {
     /// Authorization List. If this element is absent, than
     /// the Local Authorization List SHALL be deleted.
     id_tag_info: Option<IdTagInfo>,
+}
+
+pub struct IdTagInfo {
+    expiry_date: Option<DateTime<Utc>>,
+    parent_id_tag: Option<IdToken>,
+    status: AuthorizationStatus,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct IdToken {
+    id_token: String,
+}
+
+impl IdToken {
+    pub fn new(id_token: String) -> IdToken {
+        if id_token.len() > 20 {
+            panic!("Max size of 20 characters")
+        }
+
+        IdToken { id_token }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::iter;
+
+    #[test]
+    fn test_with_valid_id_token() {
+        let token = String::from("abcd-01234");
+        let id_token = IdToken::new(token);
+
+        assert_eq!(
+            id_token,
+            IdToken {
+                id_token: String::from("abcd-01234")
+            }
+        )
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_with_invalid_id_token() {
+        let token = iter::repeat("a").take(21).collect();
+        let _id_token = IdToken::new(token);
+    }
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,4 +1,3 @@
 pub mod authorization_data;
 pub mod charging_profile;
 pub mod enumerations;
-pub mod id_tag_info;
